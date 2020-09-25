a805044a03ccf8a10eeae172bd244c95f9409ea5
FELIPE ASSIS DE JESUS <45512256+lipeA@users.noreply.github.com>
2020-09-25 11:11:49 -0300

    alteração no modo calculo

--- opera.js
+++ opera.js
@@ -2,24 +2,45 @@
 const calcular = document.getElementById('calcular');
 
 function imc() {
-// acessando os campos nome, altura, peso e resultado.
-const nome = document.getElementById('nome').value;
-const altura = document.getElementById('altura').value;
-const peso = document.getElementById('peso').value;
-const resultado = document.getElementById('result').value;
+    // acessando os campos nome, altura, peso e resultado.
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('result');
 
 
-// esse primeiro if verifica se todos os campos estão preenchidos!
-if (nome !== ' ' && peso!=='' && altura !=='' ) {
-    alert('campos preenhidos')
-    
-} else {
-    resultado.
-}
+    // esse primeiro if verifica se todos os campos estão preenchidos!
+    if (nome !== ' ' && peso !== '' && altura !== '') {
 
+        const valorimc = (peso / (altura * altura)).toFixed(1);
 
+        
+
+        /*
+        let classficado = '';
+
+        if (valorIMC < 18.5) {
+            classificacao = 'abaixo do peso.';
+        } else if (valorIMC < 25) {
+            classificacao = 'com peso ideal. Parabéns!!!';
+        } else if (valorIMC < 30) {
+            classificacao = 'levemente acima do peso.';
+        } else if (valorIMC < 35) {
+            classificacao = 'com obesidade grau I.';
+        } else if (valorIMC < 40) {
+            classificacao = 'com obesidade grau II';
+        } else {
+            classificacao = 'com obesidade grau III. Cuidado!!';
+        }
+
+        resultado.textContent =  `${nome} seu IMC é ${valorimc} e vc esta ${classficado} `
+        */
+
+    } else {
+        alert('Preencha todos os campos! ')
+    }
 
 }
 
 //aqui vai escutar quando for clicado ira chamar a função imc
-calcular.addEventListener('click',imc);
+calcular.addEventListener('click', imc);
